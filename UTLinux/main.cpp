7cdd0d36eebbae2120f064819619faacb92e9762
daixian <amano_tooko@qq.com>
2019-04-04 01:02:33 +0800

    准备引用dlog试试，但是没有通过

--- UTLinux/main.cpp
+++ UTLinux/main.cpp
@@ -1,7 +1,9 @@
 #include "stdio.h"
+#include "../dlog/dlog.h"
 
 int main(int argc, char const *argv[])
 {
     printf("你好！！\r\n");
+    dlog_init("utlinux", "utlinux", dlog_init_relative::MODULE, false);
     return 0;
 }
