cbc3bac729c8419493b5ece9a915aa8f4d300ddc
daixian <amano_tooko@qq.com>
2019-04-05 18:19:50 +0800

    修改了测试方法的路径表示，它在linux下是绝对路径

--- UTCpp/DebugTest.cpp
+++ UTCpp/DebugTest.cpp
@@ -2,31 +2,29 @@
 #include "../dlog/Common/MemoryLog.h"
 #include "../dlog/Common/Debug.h"
 #include "../dlog/dlog.h"
-#include  "../dlog/Common/FileHelper.h"
+#include "../dlog/Common/FileHelper.h"
 
 #if defined(_WIN32) || defined(_WIN64)
 #pragma comment(lib, "dlog.lib")
 #elif defined(__linux__)
 #endif
 
-
 using namespace dxlib;
 using namespace std;
-
 
 TEST(Debug, LogSeverity)
 {
     size_t count;
 
     Debug::GetInst()->clear();
-    EXPECT_FALSE(Debug::GetInst()->isInit);//应该还未初始化
+    EXPECT_FALSE(Debug::GetInst()->isInit); //应该还未初始化
     EXPECT_TRUE(Debug::GetInst()->filelogger == nullptr);
 
     Debug::GetInst()->init();
-    EXPECT_TRUE(Debug::GetInst()->isInit);//应该初始化成功
+    EXPECT_TRUE(Debug::GetInst()->isInit); //应该初始化成功
     EXPECT_TRUE(Debug::GetInst()->filelogger != nullptr);
 
-   //关闭整个之后应该没有日志写入
+    //关闭整个之后应该没有日志写入
     Debug::GetInst()->isEnable = false;
     Debug::GetInst()->isMemLogEnable = true;
 
@@ -68,7 +66,6 @@ TEST(Debug, LogSeverity)
     EXPECT_TRUE(log == string("LogW"));
     MemoryLog::GetInst()->getLog(log);
     EXPECT_TRUE(log == string("LogE"));
-
 
     //打开向内存日志写入LogD
     Debug::GetInst()->isEnable = true;
--- UTCpp/dlogTest.cpp
+++ UTCpp/dlogTest.cpp
@@ -16,22 +16,19 @@ TEST(dlog, memorylog)
 {
     dlog_close();
 
-    dlog_init("\\临时测试\\log", "测试日志");
+    dlog_init("临时测试/log", "测试日志");
     dlog_memory_log_enable(true);
     dlog_set_console_thr(dlog_level::err);
     //LogI打印10W条，异步的，只要942毫秒
     //LogW打印10W条，也只要1秒的样子
-    for (size_t i = 0; i < 4096; i++)
-    {
+    for (size_t i = 0; i < 4096; i++) {
         LogW("测试日志%d !", i);
     }
 
     char msg[512];
     char msgCorr[512];
-    for (int i = 0; i < 4096; i++)
-    {
-        if (dlog_get_memlog(msg, 0, 512) > 0)
-        {
+    for (int i = 0; i < 4096; i++) {
+        if (dlog_get_memlog(msg, 0, 512) > 0) {
 #if defined(_WIN32) || defined(_WIN64)
             sprintf_s(msgCorr, 512, "测试日志%d !", i); //正确的消息应该是
 #elif defined(__linux__)
@@ -39,8 +36,7 @@ TEST(dlog, memorylog)
 #endif
             EXPECT_TRUE(strcmp(msg, msgCorr) == 0) << "msg=" << msg; //比对提取的消息是否正确
         }
-        else
-        {
+        else {
             FAIL();
         }
     }
@@ -48,27 +44,26 @@ TEST(dlog, memorylog)
 
 TEST(dlog, init_close)
 {
-    for (size_t i = 0; i < 10; i++)
-    {
+    for (size_t i = 0; i < 10; i++) {
         dlog_close();
 
         //第一次创建
-        int res = dlog_init("\\临时测试\\log", "创建测试", dlog_init_relative::MODULE);
+        int res = dlog_init("临时测试/log", "创建测试", dlog_init_relative::MODULE);
         EXPECT_TRUE(res == 0);
         LogI("132");
 
         //复用
-        res = dlog_init("\\临时测试\\log", "创建测试", dlog_init_relative::MODULE, false);
+        res = dlog_init("临时测试/log", "创建测试", dlog_init_relative::MODULE, false);
         EXPECT_TRUE(res == 1);
         LogI("132");
 
         //强制创建,因为重名所以还是复用
-        res = dlog_init("\\临时测试\\log", "创建测试", dlog_init_relative::MODULE, true);
+        res = dlog_init("临时测试/log", "创建测试", dlog_init_relative::MODULE, true);
         EXPECT_TRUE(res == 3);
         LogI("132");
 
         //强制创建
-        res = dlog_init("\\临时测试\\log", "创建测试2", dlog_init_relative::MODULE, true);
+        res = dlog_init("临时测试/log", "创建测试2", dlog_init_relative::MODULE, true);
         EXPECT_TRUE(res == 2);
         LogI("132");
     }
