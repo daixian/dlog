8d62ef195ad6643c6a746850d5995f29a2f408ea
daixian <amano_tooko@qq.com>
2019-04-17 23:58:11 +0800

    去掉了子模块

---
+++ build.py
@@ -0,0 +1,15 @@
+import urllib
+from urllib import request
+import os
+
+#下载
+dirConcur = "./concurrentqueue"
+if not os.path.exists(dirConcur):
+    os.makedirs(dirConcur)
+
+print("下载 concurrentqueue ...")
+url = 'https://github.com/cameron314/concurrentqueue/raw/master/blockingconcurrentqueue.h'
+request.urlretrieve(url, dirConcur + "/blockingconcurrentqueue.h")
+url = "https://github.com/cameron314/concurrentqueue/raw/master/concurrentqueue.h"
+request.urlretrieve(url, dirConcur + "/concurrentqueue.h")
+print("下载完成")
