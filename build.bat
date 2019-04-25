b018678714df482f1a5f8d24a4b54ead77ea79f3
daixian <amano_tooko@qq.com>
2019-04-25 17:45:22 +0800

    增加了调用cmake的命令行

--- build.bat
+++ build.bat
@@ -1,3 +1,13 @@
 python -V
-:: python build.py
-cmake.exe -G "Visual Studio 15 2017 x64"
+python build.py
+mkdir build
+cd build
+mkdir Win64
+cd Win64
+cmake.exe -G "Visual Studio 15 2017 Win64" "../../" -DBUILD_GMOCK:BOOL="0" -DINSTALL_GTEST:BOOL="0" -DBUILD_TESTING:BOOL="0" -DCMAKE_INSTALL_PREFIX:PATH="./install" -DENABLE_TEST:BOOL="1" -DCMAKE_CONFIGURATION_TYPES:STRING="Release;RelWithDebInfo" -Dgtest_force_shared_crt:BOOL="1"
+::pause
+cd ../
+mkdir Win32
+cd Win32
+cmake.exe -G "Visual Studio 15 2017" "../../" -DBUILD_GMOCK:BOOL="0" -DINSTALL_GTEST:BOOL="0" -DBUILD_TESTING:BOOL="0" -DCMAKE_INSTALL_PREFIX:PATH="./install" -DENABLE_TEST:BOOL="1" -DCMAKE_CONFIGURATION_TYPES:STRING="Release;RelWithDebInfo" -Dgtest_force_shared_crt:BOOL="1"
+::pause
