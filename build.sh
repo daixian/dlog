faa2ad52adb1e173ce1d22bd77490cecc1f27aaf
daixian <amano_tooko@qq.com>
2019-04-26 19:33:59 +0800

    去掉原先调试注释掉的东西

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-# python3 -V
-# python3 ./tools/DownloadLib.py "concurrentqueue" "spdlog" "gtest" "boost"
+python3 -V
+python3 ./tools/DownloadLib.py -p "linux" "concurrentqueue" "spdlog" "gtest" "boost"
 mkdir -p build/linux/x64
 cd build/linux/x64
 cmake -G "Unix Makefiles" "../../../" -DBUILD_GMOCK:BOOL="0" -DINSTALL_GTEST:BOOL="0" -DBUILD_TESTING:BOOL="0" -DCMAKE_INSTALL_PREFIX:PATH="./install" -DENABLE_TEST:BOOL="1" -DCMAKE_CONFIGURATION_TYPES:STRING="Release;RelWithDebInfo" -Dgtest_force_shared_crt:BOOL="1"
