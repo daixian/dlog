a2d96ae17a3e6df78b92bf29806c7f3be15140f2
daixian <amano_tooko@qq.com>
2019-11-12 04:25:53 +0800

    linux下有效

--- build.sh
+++ build.sh
@@ -6,8 +6,11 @@ python3 -V
 python3 ./tools/DownloadLib.py -d"$HOME/dxlib/download" -l"$HOME/dxlib/lib" -p"linux" "concurrentqueue"
 
 # pip3 install conan
-mkdir -p build/Linux
-cd build/Linux
-conan install ../.. --build=missing
-cmake -G "Unix Makefiles" "../.." -DBUILD_GMOCK:BOOL="0" -DINSTALL_GTEST:BOOL="0" -DBUILD_TESTING:BOOL="0" -DCMAKE_INSTALL_PREFIX:PATH="./install" -DENABLE_TEST:BOOL="1" -DCMAKE_CONFIGURATION_TYPES:STRING="Release;RelWithDebInfo" -Dgtest_force_shared_crt:BOOL="1"
-make
+conan create . demo/testing --build missing
+
+
+# mkdir -p build/Linux
+# cd build/Linux
+# conan install ../.. --build=missing
+# cmake -G "Unix Makefiles" "../.." -DBUILD_GMOCK:BOOL="0" -DINSTALL_GTEST:BOOL="0" -DBUILD_TESTING:BOOL="0" -DCMAKE_INSTALL_PREFIX:PATH="./install" -DENABLE_TEST:BOOL="1" -DCMAKE_CONFIGURATION_TYPES:STRING="Release;RelWithDebInfo" -Dgtest_force_shared_crt:BOOL="1"
+# make
