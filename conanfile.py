9df2abc6bfac54dfdd01e114a9f544c13e085490
daixian <amano_tooko@qq.com>
2021-03-25 17:15:56 +0800

    修改一下静态编译的头文件吧

--- conanfile.py
+++ conanfile.py
@@ -81,8 +81,11 @@ class DlogConan(ConanFile):
 
     def package(self):
         # 拷贝如果不带*那么不会搜索到下一级文件夹
-        # 包含所有的头文件算了
-        self.copy("*/dlog.h", dst="include", src="src", keep_path=True)
+        if self.options.shared:
+            self.copy("*/dlog.h", dst="include", src="src", keep_path=True)
+        else:
+            # 包含所有的头文件算了
+            self.copy("*.h", dst="include", src="src", keep_path=True)
         self.copy("*.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.dylib*", dst="lib", keep_path=False)
